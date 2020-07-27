bb0c43a1228744ceef1b81e7879808dd6b2dd080
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-07-27 09:52:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,8 @@ resource "azuredevops_project" "project" {
   work_item_template = var.work_item_template
 }
 
+
+
 resource "azuredevops_serviceendpoint_github" "github_serviceendpoint" {
   project_id            = azuredevops_project.project.id
   service_endpoint_name = var.service_endpoint_name
@@ -33,6 +35,67 @@ resource "azuredevops_build_definition" "nightly_build" {
     branch_name           = var.branch_name
     yml_path              = var.yml_path
     service_connection_id = azuredevops_serviceendpoint_github.github_serviceendpoint.id
+  }
+}
+
+
+resource "azuredevops_git_repository" "repo" {
+  project_id = azuredevops_project.project.id
+  name       = "Sample_clone"
+  # parent_id  = data.azuredevops_git_repositories.single_repo.id
+  initialization {
+    init_type = "Clean"
+  }
+}
+
+resource "azuredevops_branch_policy_build_validation" "repo_build_policy" {
+  count      = var.enable_repo_build_policy ? 1 : 0
+  project_id = azuredevops_project.project.id
+
+  enabled  = true
+  blocking = true
+
+  settings {
+    display_name        = "Don't break the build!"
+    build_definition_id = azuredevops_build_definition.nightly_build.id
+    valid_duration      = 0
+
+    scope {
+      repository_id  = azuredevops_git_repository.repo.id
+      repository_ref = azuredevops_git_repository.repo.default_branch
+      match_type     = "Exact"
+    }
+
+    scope {
+      repository_id  = azuredevops_git_repository.repo.id
+      repository_ref = "refs/heads/releases"
+      match_type     = "Prefix"
+    }
+  }
+}
+
+resource "azuredevops_branch_policy_min_reviewers" "repo_build_reviewers" {
+  count      = var.enable_repo_build_reviewers ? 1 : 0
+  project_id = azuredevops_project.project.id
+
+  enabled  = true
+  blocking = true
+
+  settings {
+    reviewer_count     = var.policy_reviewer_count
+    submitter_can_vote = false
+
+    scope {
+      repository_id  = azuredevops_git_repository.repo.id
+      repository_ref = azuredevops_git_repository.repo.default_branch
+      match_type     = "Exact"
+    }
+
+    scope {
+      repository_id  = azuredevops_git_repository.repo.id
+      repository_ref = "refs/heads/releases"
+      match_type     = "Prefix"
+    }
   }
 }
 
